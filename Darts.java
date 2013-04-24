851836bd32003fd5a3eff3d433fc1675ec097e65
andycornforth <andycornforth@live.co.uk>
2013-04-24 13:28:01 +0200

    Create Darts.java
    the main

---
+++ Darts.java
@@ -0,0 +1,15 @@
+
+package darts;
+
+/**
+ *
+ * @author Andy
+ */
+public class Darts {
+
+    public static void main(String[] args) {       
+        
+        StartScreen gui = new StartScreen();
+        
+    }
+}
